d8b0deb80136bb3d896992914a256f4623da25a3
CMDR Axle Antilles <axleantilles@proton.me>
2025-11-20 01:30:57 -0800

    Consolidate constants
    fixes #2
    
    Provides centralized repository of common constants with abstraction of dev/prod

---
+++ ptn_utils/global_constants/prod/generic.py
@@ -0,0 +1,39 @@
+# Define whether the bot is in testing or live mode. Default is testing mode.
+import discord
+import os
+
+from dotenv import load_dotenv
+
+
+# define paths
+TESTING_DATA_PATH = os.getcwd()  # defines the path for use in a local testing environment
+DATA_DIR = os.getenv("DATA_DIR", TESTING_DATA_PATH)
+
+# Get the discord token from the local .env file. Deliberately not hosted in the repo or Discord takes the bot down
+# because the keys are exposed. DO NOT HOST IN THE PUBLIC REPO.
+# load_dotenv(os.path.join(DATA_DIR, '.env'))
+load_dotenv(os.path.join(DATA_DIR, ".env"))
+
+# define bot token
+TOKEN = os.getenv("DISCORD_TOKEN_PROD")
+
+############################
+### Production variables ###
+############################
+"""
+For consistency, please prepend all emoji with "EMOJI_"
+"""
+
+DISCORD_GUILD = 800080948716503040  # PTN server ID
+guild_obj = discord.Object(DISCORD_GUILD)
+
+# Emoji
+EMOJI_ASSASSIN = 806498760586035200  # PTN :assassin: emoji
+EMOJI_CARRIER_DONE = 878216234653605968  # PTN :fc_complete: emoji
+EMOJI_CARRIER_EMPTY = 878216288525242388  # PTN :fc_empty: emoji
+EMOJI_DISCORD_EMOJI = 1122605426844905503  # PTN :discord: emoji
+EMOJI_LOADING_ICON = 1160871046786846780  # PTN :loading_icon: emoji
+EMOJI_O7 = 806138784294371368  # PTN :o7: emoji
+EMOJI_PTN_ROLE_ICON = 1109925017443115088  # PTN :PTN_roleicon: emoji
+EMOJI_THOON = 1058010828458176563  # PTN :thoon: emoji
+EMOJI_UNLOADING_ICON = 1160881163641049178  # PTN :unloading_icon: emoji
