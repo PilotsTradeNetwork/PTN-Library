e3526e3231ebfaaf52150ae6385ff49732308e84
CMDR Axle Antilles <axleantilles@proton.me>
2026-02-21 01:50:14 -0800

    ignore ruff errors related to star imports in global_constants

--- ptn_utils/global_constants/dev/__init__.py
+++ ptn_utils/global_constants/dev/__init__.py
@@ -1,3 +1,4 @@
+# ruff: noqa: F403
 from ptn_utils.global_constants.dev.channels import *
 from ptn_utils.global_constants.dev.generic import *
 from ptn_utils.global_constants.dev.roles import *
--- ptn_utils/global_constants/prod/__init__.py
+++ ptn_utils/global_constants/prod/__init__.py
@@ -1,3 +1,4 @@
+# ruff: noqa: F403
 from ptn_utils.global_constants.prod.channels import *
 from ptn_utils.global_constants.prod.generic import *
 from ptn_utils.global_constants.prod.roles import *
